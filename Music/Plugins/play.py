a900ebb44c554196f1c4cde5ae0d27383ceafb0b
Shubhanshutya <90434291+Amantya@users.noreply.github.com>
2021-11-27 22:16:29 +0530

    Update play.py

--- Music/Plugins/play.py
+++ Music/Plugins/play.py
@@ -54,7 +54,7 @@ def time_to_seconds(time):
         int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":")))
     )
 
-@Client.on_message(command(["play", "play@Tg_Vc_00_Bot"]))
+@Client.on_message(command(["play", "play@MentosMusicBot)
 async def play(_, message: Message):
     chat_id = message.chat.id
 #   if not await is_served_chat(chat_id):
